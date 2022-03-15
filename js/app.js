8253c4e3e0b513c1b45fd42043cee4827198a3b9
Muhajirul Faqih <muhajirulfaqih@gmail.com>
2022-03-15 08:05:58 +0700

    Update

--- js/app.js
+++ js/app.js
@@ -10,4 +10,9 @@
     ACTparallax();
     AOS.init({offset: 0, duration: 800});
 
+    $('#video-modal').on('hide.bs.modal', function() {
+        var memory = $(this).html();
+        $(this).html(memory);
+   });
+
 })(window.jQuery);
